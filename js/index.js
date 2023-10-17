154a0447ab6a1609627047e35bf833aba636dd77
n0urs <20nourabdulnasser00@gmail.com>
2023-10-17 03:38:03 +0300

    updated index.js: grabbing values outside the function

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 
-const  num_length = document.getElementById("numbers").value;
-const  time_length = document.getElementById("time").value;
 
-var num = document.getElementById("display");
+function random_num(num_length) {
+    return Math.floor(Math.pow(10, num_length-1) + Math.random() * (Math.pow(10, num_length) - Math.pow(10, num_length-1) - 1));
+   
+}
 
 function num_mem_init(num_length, time_length){
-    function random_num(num_length) {
-        return Math.floor(Math.pow(10, num_length-1) + Math.random() * (Math.pow(10, num_length) - Math.pow(10, num_length-1) - 1));
-       
-    }
+    const  num_length = document.getElementById("numbers").value;
+    const  time_length = document.getElementById("time").value;
+    var num = document.getElementById("display");
+
     num.innerHTML = random_num(num_length);
     setTimeout(
         function()
