d569568d151069860ea74ef163fcde8a1130b087
n0urs <20nourabdulnasser00@gmail.com>
2023-10-20 01:11:58 +0300

    fixed too many digits bug

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-
-
-function random_num(num_length) {
+function random_num() {
+    const num_length = 1;
     return Math.floor(Math.pow(10, num_length-1) + Math.random() * (Math.pow(10, num_length) - Math.pow(10, num_length-1) - 1));
-   
+}
+
+function random_nums(){
+    let num_string = "";
+    var  num_length = document.getElementById("numbers").value;
+    for (let i = 0; i < num_length; i++){
+        num_string = num_string + random_num().toString();
+    }
+    return num_string;
 }
 
 function num_mem_init(){
@@ -10,12 +17,11 @@ function num_mem_init(){
     const  time_length = document.getElementById("time").value;
     var num = document.getElementById("display");
 
-    num.innerHTML = random_num(num_length);
+    num.innerHTML = random_nums(num_length);
     setTimeout(
         function()
         {
         num.innerHTML = '';
         }
         , time_length*1000);
-    
 }
