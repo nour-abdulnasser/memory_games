9bedb3217cea494b4d84d8b0b23e10167ea340c5
n0urs <20nourabdulnasser00@gmail.com>
2023-10-20 05:35:33 +0300

    added feature: check if input memorised number matches generated number

--- js/index.js
+++ js/index.js
@@ -1,27 +1,60 @@
+
+
+var randy = "";
+
+
 function random_num() {
     const num_length = 1;
     return Math.floor(Math.pow(10, num_length-1) + Math.random() * (Math.pow(10, num_length) - Math.pow(10, num_length-1) - 1));
 }
 
 function random_nums(){
-    let num_string = "";
-    var  num_length = document.getElementById("numbers").value;
+    const  num_length = document.getElementById("numbers").value;
     for (let i = 0; i < num_length; i++){
-        num_string = num_string + random_num().toString();
+        randy = randy + random_num().toString();
     }
-    return num_string;
+    return randy;
 }
 
 function num_mem_init(){
-    const  num_length = document.getElementById("numbers").value;
+    // const  num_length = document.getElementById("numbers").value;
     const  time_length = document.getElementById("time").value;
     var num = document.getElementById("display");
-
-    num.innerHTML = random_nums(num_length);
+    // randy = random_nums();
+    num.innerHTML = random_nums();
     setTimeout(
         function()
         {
-        num.innerHTML = '';
+        num.innerHTML = "Time's up!";
         }
-        , time_length*1000);
+        , time_length*1000); 
+    
 }
+
+function check(){
+    const input_num = document.getElementById("input_test").value;
+    // const  num_length = document.getElementById("numbers").value;
+
+    if (input_num == randy) {
+        document.getElementById("check_disp").innerHTML = "well done";
+    } else {
+        document.getElementById("check_disp").innerHTML = "nope. good luck next time";
+    }
+
+
+    // for (let i=0; i < num_length; i++ ){
+    //     if (input_num == randy) {
+    //         document.getElementById("check_disp") = 
+    //     }
+    // }
+
+}
+
+
+
+
+
+
+
+
+
