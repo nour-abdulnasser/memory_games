18c2aa2aed11cc05a53938d914ee2d6dc2abae24
n0urs <20nourabdulnasser00@gmail.com>
2023-10-21 09:39:55 +0300

    cleaner interface

--- js/index.js
+++ js/index.js
@@ -19,22 +19,32 @@ function random_nums(){
 
 
 function num_mem_init(){
-    const  num_length = document.getElementById("numbers").value;
     const  time_length = document.getElementById("time").value;
     var butt = document.getElementById('start_button');
     butt.style.display = 'none';
+    document.getElementById("display").style.display='block';
+    document.getElementById("time").style.display='none';
+    document.getElementById("time_label").style.display='none';
+    document.getElementById("numbers").style.display='none';
+    document.getElementById("numbers_label").style.display='none';
+    document.getElementById("check_disp").innerHTML="";
+
     var num = document.getElementById("display");
     // randy = random_nums();
     num.innerHTML = random_nums();
-    document.getElementById("numbers").value="";
-    document.getElementById("time").value="";
+    // randy = "";
+
+    // document.getElementById("numbers").value="";
+    // document.getElementById("time").value="";
     // localStorage.setItem('mr_randy', num.innerHTML); // used for storing value to pass between pages
     setTimeout(
         function()
         {
         num.innerHTML = "Time's up! What's the number?";
-        // document.getElementById("numbers").value="";
-        // document.getElementById("time").value="";
+        document.getElementById("input_test").style.display="inline";
+        document.getElementById("submit_button").style.display="inline";
+        document.getElementById("numbers").value="";
+        document.getElementById("time").value="";
         }
         , time_length*1000);
 }
@@ -46,6 +56,14 @@ function num_mem_init(){
 function check(){
     var butt = document.getElementById('start_button');
     const input_num = document.getElementById("input_test").value;
+    document.getElementById("input_test").style.display="none";
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("display").style.display='none';
+    document.getElementById("time").style.display='inline';
+    document.getElementById("time_label").style.display='inline';
+    document.getElementById("numbers").style.display='inline';
+    document.getElementById("numbers_label").style.display='inline';
+    
     // const  num_length = document.getElementById("numbers").value;
 
     if (input_num == randy) {
@@ -54,8 +72,9 @@ function check(){
         document.getElementById("check_disp").innerHTML = "nope. good luck next time";
     }
     document.getElementById("input_test").value="";
+    // document.getElementById("display").style.display='none';
     butt.style.display = 'inline';
-
+    randy ="";
     // for (let i=0; i < num_length; i++ ){
     //     if (input_num == randy) {
     //         document.getElementById("check_disp") = 
